24563ae3612a73a5550e47442108bc3f623f9372
Peter Constable <peterconstable@kindle.com>
2015-11-21 09:44:31 -0800

    Initial commit on GitHub

---
+++ VMG Finder/VMGFinderModel.h
@@ -0,0 +1,13 @@
+//
+//  VMGFinderModel.h
+//  VMG Finder
+//
+//  Created by Peter Constable on 1/12/14.
+//  Copyright (c) 2014 Peter Constable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VMGFinderModel : NSObject
+
+@end
---
+++ VMG Finder/VMGFinderModel.m
@@ -0,0 +1,13 @@
+//
+//  VMGFinderModel.m
+//  VMG Finder
+//
+//  Created by Peter Constable on 1/12/14.
+//  Copyright (c) 2014 Peter Constable. All rights reserved.
+//
+
+#import "VMGFinderModel.h"
+
+@implementation VMGFinderModel
+
+@end
